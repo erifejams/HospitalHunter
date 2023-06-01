758bd5fefb9db7c97116507bb01cf94777be0a1c
Erifeoluwa Jamgbadi <44726422+erifejams@users.noreply.github.com>
2023-06-01 10:28:10 +0100

    robot can avoid objects - needs more work
    - added the script that allows the robot to change directions and avoid objects, problem is that it goes past some objects as there is space under those objects, so can't sense the items

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from glob import glob
+
+
+package_name = 'HospitalHunter'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+
+        ## NOTE: you must add this line to use launch files
+        # Instruct colcon to copy launch files during package build 
+        ('share/' + package_name + '/launch', glob('launch/*.launch.*'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    #maintainer='robotics23',
+    #maintainer_email='robotics23@todo.todo',
+    maintainer='usi',
+    maintainer_email='usi@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            #'controller_node = HospitalHunter.controller_node:main',
+            'controller_node3 = HospitalHunter.controller_node3:main'
+        ],
+    },
+)
